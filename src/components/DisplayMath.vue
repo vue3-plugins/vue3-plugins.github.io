f93552372265b854b905557d7d7ac2bf8075bc41
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-11-05 12:54:16 +1300

    Add auto directive use example.

--- src/components/DisplayMath.vue
+++ src/components/DisplayMath.vue
@@ -15,6 +15,8 @@ const mathText = '\\frac{c_i}{1+x}'
   <div>The following should be a math equation:</div>
   <div v-katex:display="'\\frac{a_i}{1+x}'"></div>
   <div>Next we try an inline math equation: <span v-katex:inline="'\\frac{b_i}{1+x}'"></span>.</div>
+  <div>We should also try out the auto display option:<span v-katex:auto>\(\frac{d_i}{1+x}\)</span>, no need to escape \ here.</div>
+  
   <div>Can we use the KatexElement?</div>
   <KatexElement :expression="mathText" :displayMode="true"/>
 </template>
