994de7caa18892a27a927f94fcba22a96d4533d0
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-11-10 15:00:29 +1300

    Manually send event on page load.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup>
-  import DisplayMath from './components/DisplayMath.vue';
-  import DisplayChemistry from './components/DisplayChemistry.vue';
+  import { onMounted } from 'vue'
+  import { event } from 'vue-gtag'
+
+  import DisplayMath from './components/DisplayMath.vue'
+  import DisplayChemistry from './components/DisplayChemistry.vue'
+
+  onMounted(() => {
+    console.log(`the component is now mounted.`)
+    event('page_mounted', { name: 'main' })
+  })
+  
 </script>
 
 <template>
